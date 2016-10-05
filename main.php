1573a25a86cefc7207bd9b11fdeabee3b04f3a4d
Forrestbob <forrestbob2000@yahoo.com>
2016-10-05 02:55:26 -0400

    initial commit

---
+++ main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Project 2</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<div class = "hangman">
+
+</div>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
