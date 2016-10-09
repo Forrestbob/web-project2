2a415998871ef469b131cfb2c78704a53b11f96e
Forrestbob <forrestbob2000@yahoo.com>
2016-10-08 23:18:56 -0400

    removed template

--- hangman.php
+++ hangman.php
@@ -1,86 +1,18 @@
 <?php
 	session_start();
-
-//$_SESSION['wins']+=2;
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-
-<link rel="stylesheet" type="text/css" href="styles.css" media="screen"/>
-<title>HANGMAN</title>
+  <title>Title of document</title>
 </head>
 
 <body>
-
-<div id="container">
-
-	<div class="left" id="main_left">
-
-		<div id="header"><img src="images/Hangman-6.png" alt="hangman6"></div>
-			
-		<div class="right" id="main">
-
-			<div class="padded">
-
-				<h1>HANGY</h1>
-				
-				<div class="content_separator"></div>
-				
-				
-				<div class="clearer">&nbsp;</div>
-
-				<div class="content_separator"></div>
-
-				<h2>insert game here</h2>
-
-				<div class="success">You win!</div>
-
-				<div class="error">Loser</div>
-
-				<div class="content_separator"></div>
-
-							
-			</div>
-
-		</div>
-
-
-		<div class="right" id="main_right">
-
-		<div class="padded">
-			
-			<h4>SCORE</h4>
-			<h5>Wins:</h5>
-			<p><?php echo $_SESSION['wins'];?></p>
-			
-			<h5>Losses:</h5>
-			<p><?php echo $_SESSION['losses'];?></p>
-		</div>
-<p> Template credit: http://creativecommons.org/licenses/by/2.5/
-Website: http://arcsin.se/ <p>
-	</div>
-	
-		<div class="clearer">&nbsp;</div>
-
-	</div>
-
-	<div class="right" id="main_right">
-
-		<div class="padded">
-			
-			
-		</div>
-
-	</div>
-
-	<div class="clearer">&nbsp;</div>
-
-
-</div>
-
+  some content 
 </body>
 
 </html>
--- main.php
+++ main.php
@@ -12,97 +12,19 @@ unset($_SESSION['hangs']);
 unset($_SESSION['current_phrase']);
 unset($_SESSION['alphabet']);
 
-//$_SESSION['wins']+=2;
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-
-<link rel="stylesheet" type="text/css" href="styles.css" media="screen"/>
-<title>Project 2</title>
+  <title>Title of document</title>
 </head>
 
 <body>
-
-<div id="container">
-
-	<div class="left" id="main_left">
-
-		<div id="header">Let's play Hang Man!</div>
-			
-		<div class="right" id="main">
-
-			<div class="padded">
-
-				<h1>HANGY</h1>
-				
-				<div class="content_separator"></div>
-				
-				
-				<div class="clearer">&nbsp;</div>
-
-				<div class="content_separator"></div>
-
-				<h2>insert game here</h2>
-
-				<div class="success">You win!</div>
-
-				<div class="error">Loser</div>
-
-				<div class="content_separator"></div>
-
-							
-			</div>
-
-		</div>
-
-		<div class="left" id="subnav">
-
-			<h1>Choose a topic!</h1>
-			<ul>
-			<li><a href="test.php?topic=languages">Programming Languages</a></li>
-				<li><a href="test.php?topic=professors">Cool Professors</a></li>
-				<li><a href="test.php?topic=buildings">GSU Buildings</a></li>
-				<li><a href="test.php?topic=legends">Legends</a></li>
-				<li><a href = "test.php?topic=majors"> Unprofitable Majors</a></li>
-			</ul>
-
-		</div>
-		<div class="right" id="main_right">
-
-		<div class="padded">
-			
-			<h4>SCORE</h4>
-			<h5>Wins:</h5>
-			<p><?php echo $_SESSION['wins'];?></p>
-			
-			<h5>Losses:</h5>
-			<p><?php echo $_SESSION['losses'];?></p>
-		</div>
-<p> Template credit: http://creativecommons.org/licenses/by/2.5/
-Website: http://arcsin.se/ <p>
-	</div>
-	
-		<div class="clearer">&nbsp;</div>
-
-	</div>
-
-	<div class="right" id="main_right">
-
-		<div class="padded">
-			
-			
-		</div>
-
-	</div>
-
-	<div class="clearer">&nbsp;</div>
-
-
-</div>
-
+  some content 
 </body>
 
 </html>
